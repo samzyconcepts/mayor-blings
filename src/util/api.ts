5dd2016e414eea445a1440b3725153077378338a
samzyconcepts <samson.oderinwale@kibo.school>
2024-05-11 23:05:25 +0100

    API: create file for all the API URL to be consumed

---
+++ src/util/api.ts
@@ -0,0 +1,19 @@
+const base_url = "https://mayor-blings.onrender.com/";
+
+// get categories List
+export const getCategoriesURL = () => `${base_url}categories`;
+
+//  get a category detail
+export const getCategoryURL = (category_id: number) =>
+    `${base_url}categories/category?category_id=${category_id}`;
+
+//  get products in a category
+export const getCategoryProductURL = (category_id: number) =>
+    `${base_url}category/products?category_id=${category_id}`;
+
+// get all products
+export const getProductsURL = () => `${base_url}products`;
+
+// get a product by id
+export const getProductURL = (product_id: number) =>
+    `${base_url}products/product?product_id=${product_id}`;
