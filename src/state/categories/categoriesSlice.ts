c198f1ca62b33b0ad96c61985c2aa6bac34b3699
samzyconcepts <samson.oderinwale@kibo.school>
2024-05-11 23:11:41 +0100

    Redux: categories slice to get all categories and store it

---
+++ src/state/categories/categoriesSlice.ts
@@ -0,0 +1,40 @@
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import axios from "axios";
+import { getCategoriesURL } from "../../util/api";
+
+// interface Category {
+//     id: number;
+//     category_name: string;
+//     category_description: string;
+//     category_image: string;
+//     created_at: string;
+//     updated_at: string;
+// }
+
+interface CategoriesState {
+    categories: [];
+}
+
+const initialState: CategoriesState = {
+    categories: [],
+};
+
+const categoriesSlice = createSlice({
+    name: "categories",
+    initialState,
+    reducers: {},
+    extraReducers(builder) {
+        builder.addCase(getCategoriesAsync.fulfilled, (state, action: PayloadAction<[]>) => {
+            return { ...state, categories: action.payload };
+        });
+    },
+});
+
+export const getCategoriesAsync = createAsyncThunk("categories/getCategoriesAsync", async () => {
+    const res = await axios.get(`${getCategoriesURL()}`);
+    const categories = await res.data;
+
+    return categories;
+});
+
+export default categoriesSlice.reducer;
