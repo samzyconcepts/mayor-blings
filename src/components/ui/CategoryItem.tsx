fe3f0e21188752c89bb723ef53d15c14f3fabd5f
samzyconcepts <samson.oderinwale@kibo.school>
2024-05-12 22:10:54 +0100

    style: updated the styles

--- src/components/ui/CategoryItem.tsx
+++ src/components/ui/CategoryItem.tsx
@@ -1,16 +1,18 @@
+import { Link } from "react-router-dom";
 interface CategoryProp {
+    id: number;
     imgUrl: string;
     categoryName: string;
 }
 
-const Category = ({ imgUrl, categoryName }: CategoryProp) => {
+const Category = ({ id, imgUrl, categoryName }: CategoryProp) => {
     return (
-        <div className="w-fit">
+        <Link to={`/category/${id}`} className="w-fit">
             <div className="w-72 h-80 overflow-hidden">
                 <img src={imgUrl} alt={imgUrl} className="object-cover w-full h-full" />
             </div>
             <h1 className="text-center capitalize py-4">{categoryName}</h1>
-        </div>
+        </Link>
     );
 };
 export default Category;
--- src/components/ui/Product.tsx
+++ src/components/ui/Product.tsx
@@ -8,12 +8,12 @@ interface ProductProp {
 
 const Product = ({ imgUrl, name, price }: ProductProp) => {
     return (
-        <div className="p-4 rounded-md bg-grey-50 w-fit">
-            <div className="w-36 h-40 md:w-40 lg:w-72  lg:h-80 overflow-hidden rounded">
+        <div className="p-4 rounded-md bg-grey-50 w-full">
+            <div className="w-full h-48  lg:h-80 overflow-hidden rounded">
                 <img src={imgUrl} alt={imgUrl} className="object-cover w-full h-full" />
             </div>
-            <h1 className=" capitalize py-4">{name}</h1>
-            <div className="flex gap-2 items-center">
+            <h1 className="capitalize py-4">{name}</h1>
+            <div className="flex flex-col md:flex-row gap-2 md:items-center">
                 <p>$ {price}</p>
                 <Button>Add to Cart</Button>
             </div>
