bbe7c2236099bae32e8689f8fb0dc200f17c65bb
samzyconcepts <samson.oderinwale@kibo.school>
2024-05-14 20:42:10 +0100

    cart store: added cart reducer to the redux store

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,16 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
 import categoriesReducer, { getCategoriesAsync } from "./categories/categoriesSlice";
-import productsReducer, { getProductsAsync } from "./products/productsSlice"
+import productsReducer, { getProductsAsync } from "./products/productsSlice";
+import cartReducer from "./cart/cartSlice";
 
 export const store = configureStore({
     reducer: {
         categories: categoriesReducer,
         products: productsReducer,
+        cart: cartReducer,
     },
 });
 
 store.dispatch(getCategoriesAsync());
-store.dispatch( getProductsAsync());
+store.dispatch(getProductsAsync());
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
