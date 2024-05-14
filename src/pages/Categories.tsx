2d0e8428f4d72268bcc9ae839f1a7c8577cbc3c2
samzyconcepts <samson.oderinwale@kibo.school>
2024-05-14 21:12:34 +0100

    category: logic to change category name when the sidebar is clicked on

--- src/pages/Categories.tsx
+++ src/pages/Categories.tsx
@@ -1,20 +1,28 @@
+import { Outlet, useParams } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Sidebar from "../components/Sidebar";
-import CategoryItemsLayout from "../components/CategoryItemsLayout";
 import Footer from "../components/Footer";
+import { useSelector } from "react-redux";
+import { RootState } from "../state/store";
 
 function Categories() {
+    const { categoryId } = useParams();
+    const categories = useSelector((state: RootState) =>
+        state.categories.categories.filter(({ id }) => id == categoryId)
+    );
     return (
         <>
             <Navbar />
             <div className="container mx-auto pt-10 pb-3 px-2 md:px-0 border-b-2">
                 <h1 className="font-medium text-3xl">CATEGORIES</h1>
-                <h3 className="text-xl uppercase mt-2">Rings</h3>
+                <h3 className="text-xl uppercase mt-2">
+                    {categories.map(({ category_name }) => category_name)}
+                </h3>
             </div>
 
             <section className="container mx-auto py-5 md:py-10 grid grid-cols-1 md:grid-cols-5 border-b-2">
                 <Sidebar />
-                <CategoryItemsLayout />
+                <Outlet />
             </section>
             <Footer />
         </>
