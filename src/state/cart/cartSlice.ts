1b3cca05fe48e225338c69722070e366da93cef8
samzyconcepts <samson.oderinwale@kibo.school>
2024-05-20 20:54:25 +0100

    Cart : storing the cart in the local storage

--- src/state/cart/cartSlice.ts
+++ src/state/cart/cartSlice.ts
@@ -10,9 +10,32 @@ interface CartState {
     items: CartItem[];
 }
 
-const initialState: CartState = {
-    items: [],
+// Function to load cart from localStorage
+const loadCart = (): CartState => {
+    try {
+        const serializedState = localStorage.getItem("cart");
+
+        if (serializedState === null) {
+            return { items: [] };
+        }
+
+        return JSON.parse(serializedState);
+    } catch (error) {
+        return { items: [] };
+    }
 };
+
+// Function to state to the localStorage
+const saveState = (state: CartState) => {
+    try {
+        const serializedState = JSON.stringify(state);
+        localStorage.setItem("cart", serializedState);
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+const initialState: CartState = loadCart();
 
 const cartSlice = createSlice({
     name: "cart",
@@ -30,9 +53,12 @@ const cartSlice = createSlice({
                 //else push new item into the array of items
                 state.items.push(action.payload);
             }
+
+            saveState(state);
         },
         removeItem: (state, action: PayloadAction<number>) => {
             state.items = state.items.filter((item) => item.id !== action.payload);
+            saveState(state);
         },
         updateItemQuantity: (state, action: PayloadAction<{ id: number; quantity: number }>) => {
             const { id, quantity } = action.payload;
@@ -41,9 +67,13 @@ const cartSlice = createSlice({
             if (existingItem) {
                 existingItem.quantity = quantity;
             }
+
+            saveState(state);
         },
         clearCart: (state) => {
             state.items = [];
+
+            saveState(state);
         },
     },
 });
