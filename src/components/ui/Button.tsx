8085db6132a1b7653161bf4297ab9dc8c4d555fb
samzyconcepts <samson.oderinwale@kibo.school>
2024-06-02 15:56:31 +0100

    created new variant ghost

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -2,7 +2,7 @@ import { cva } from "class-variance-authority";
 import { cn } from "../../util/cn";
 
 type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
-    variant?: "primary" | "secondary";
+    variant?: "primary" | "secondary" | "ghost";
     type?: React.ButtonHTMLAttributes<HTMLButtonElement>["type"];
 };
 
@@ -19,6 +19,7 @@ const buttonVariants = cva(
                     "border border-grey-200 bg-grey-300 hover:bg-grey-400 text-grey-50 shadow-[0px_0px_0px_1.5px_#333,0px_1px_0px_1.5px_#333,0px_3px_0px_0px_#000000]",
                 secondary:
                     "border border-grey-400 bg-white hover:bg-grey-50 text-grey-300 shadow-[inset_0px_-3px_0px_0px_#b5b5b5]",
+                ghost: "border-none bg-none hover:bg-grey-50 text-sm"
             },
         },
         defaultVariants: {
